7294bb3e146209f162313b7fb804b6a106ddc560
matbarg <mateo.barreragarcia@outlook.com>
2026-04-10 15:04:23 +0200

    Coin spawn and collection

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    public Transform[] spawnPoints;
+    public float respawnDelay = 2f;
+
+    private GameObject currentCoin;
+
+    void Start()
+    {
+        SpawnCoin();
+    }
+
+    public void OnCoinCollected()
+    {
+        StartCoroutine(Respawn());
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        SpawnCoin();
+    }
+
+    void SpawnCoin()
+    {
+        Transform spawn = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        currentCoin = Instantiate(coinPrefab, spawn.position, Quaternion.identity);
+
+        Coin coinScript = currentCoin.GetComponent<Coin>();
+        coinScript.spawner = this;
+        
+        Debug.Log("Spawned Coin");
+    }
+}
